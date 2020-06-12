905da3af2157c0382a7958d8361d5b36dd2cb75b
Weijing Lin <weijingjaylin@gmail.com>
2020-06-12 16:28:48 -0700

    Updates

---
+++ precache-manifest.5db83273412d00b09ce396be4ba55c8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea04308765dc1dab6af3e32d9ae7d68b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4643c01323407d5f36a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f6de9330b9a342b936f7",
+    "url": "/static/js/2.4d9517e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4d9517e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4643c01323407d5f36a",
+    "url": "/static/js/main.a509d143.chunk.js"
+  },
+  {
+    "revision": "b300959e278f9f0859b9",
+    "url": "/static/js/runtime-main.9cd23b97.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

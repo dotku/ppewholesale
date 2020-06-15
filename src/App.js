871c14025e164766a20df770cb890649490bf3b4
Weijing Lin <weijingjaylin@gmail.com>
2020-06-15 00:53:00 -0700

    add divider

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import "./App.css";
 import Providers from "./components/providers";
 import Footer from "./components/common/footer";
 import Top from "./components/common/top";
+import { Typography, Divider } from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
       <Top />
       <div style={{padding: "20px"}}>
-        Please contact jay.lin@ppewholesale.us or wechat: weijingjaylin for more
-        detail.
+        <Typography variant="h6">About</Typography>
+        <p>We provide varied PPE products and support global needs.</p>
+        <Divider />
+        <Typography variant="h6">Providers</Typography>
         <Providers />
+        <Divider />
+        <Typography variant="h6">Contact</Typography>
+        <p>Please contact for more detail:</p>
+        <ul>
+          <li>Email: <a href="mailto:jay.lin@ppewholesale.us">jay.lin@ppewholesale.us</a></li>
+          <li>wechat: weijingjaylin</li>
+        </ul>
+        
       </div>
       
       <Footer />

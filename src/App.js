795ba6363732e75d003f61c22dbd1b1468b18cac
Weijing Lin <weijingjaylin@gmail.com>
2020-06-14 23:24:11 -0700

    add footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Providers from './components/providers';
+import Footer from './components/common/footer';
 
 function App() {
   return (
     <div className="App">
       <Providers />
+      <Footer />
     </div>
   );
 }

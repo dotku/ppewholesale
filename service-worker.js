e350a24429711dacdfe4fda4edd2f1fa61155bf8
dotku <weijingjaylin@gmail.com>
2020-08-06 12:08:17 +0000

    Deploying to gh-pages from  @ 13bb20bfcb09f17498999b4edecab9366187c5a2 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a13a0526759665a52fe41a7e50360b33.js"
+  "/precache-manifest.8612c1e025bf48a78b9e3311dd260600.js"
 );
 
 self.addEventListener('message', (event) => {

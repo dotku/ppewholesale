e37bbd9fbac9da3ccbf650fc9067cdd3baf9e73a
Weijing Lin <weijingjaylin@gmail.com>
2020-08-08 04:31:20 -0700

    update donate button image

--- src/components/vendors/gloves.js
+++ src/components/vendors/gloves.js
@@ -119,7 +119,7 @@ export default function Gloves() {
         <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input type="hidden" name="cmd" value="_s-xclick" />
 <input type="hidden" name="hosted_button_id" value="7LN33PXGD66Y4" />
-<input type="image" src="https://picresize.com/images/rsz_1rsz_5f2e8ac531ed8.png" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+<input type="image" src="https://aomwwf.org/images/home/support-donate1.jpg" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
 <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
 </form></center>
         </Grid>

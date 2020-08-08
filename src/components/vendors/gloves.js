c2e1f62fe079db144d59aa320d85181bfa116344
Weijing Lin <weijingjaylin@gmail.com>
2020-08-08 04:26:07 -0700

    add donate button

--- src/components/vendors/gloves.js
+++ src/components/vendors/gloves.js
@@ -115,6 +115,13 @@ export default function Gloves() {
           <Scatter name="Intco" data={data2} fillOpacity={0.5} fill={colors[getIdByName("Intco") % colors.length]} />
           <Scatter name="Cranberry" data={dataCranberry} fillOpacity={0.5} fill={colors[getIdByName("Cranberry") % colors.length]} />
         </ScatterChart>
+        <center style={{margin: "40px"}}>
+        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick" />
+<input type="hidden" name="hosted_button_id" value="7LN33PXGD66Y4" />
+<input type="image" src="https://picresize.com/images/rsz_1rsz_5f2e8ac531ed8.png" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+</form></center>
         </Grid>
       </Grid>
       

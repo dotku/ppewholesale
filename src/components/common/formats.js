680e053ed17c4415a9a51d1583f738f0bcaae4f9
Weijing Lin <weijingjaylin@gmail.com>
2020-10-10 16:32:41 -0700

    cleanup console log

--- src/components/common/formats.js
+++ src/components/common/formats.js
@@ -1,10 +1,11 @@
 function NumberFormat({ value }) {
   return new Intl.NumberFormat("en-US").format(value);
 }
-function CurrencyFormat({ value }) {
+function CurrencyFormat({ value, signDisplay = "auto" }) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
+    signDisplay,
   }).format(value);
 }
 export { NumberFormat, CurrencyFormat };

fc91c282290da8c6e2bcb9c8bc873e0b123869e5
Weijing Lin <weijingjaylin@gmail.com>
2020-10-10 23:54:41 -0700

    firebase init

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import React from "react";
 import "./App.css";
-import Top from "./components/common/top";
+import "firebase/auth";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import firebase from "firebase/app";
+import firebaseConfig from "./config/firebase";
 import Footer from "./components/common/footer";
+import React from "react";
+import rootReducer from "./reducers";
 import RouterIndex from "./config/router";
+import thunkMiddleware from "redux-thunk";
+import Top from "./components/common/top";
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+firebase.auth().onAuthStateChanged((rspUser) => {
+  store.dispatch({ type: "LOGIN", payload: rspUser });
+});
 
 function App() {
   // const body = `proudct:%0D%0Anumber in need:%0D%0Atargeted
   // price:%0D%0Atargeted location:%0D%0A`;
   // const deviceType = parser(req.headers['user-agent']).device.type || 'desktop';
-
   return (
-    <div className="App">
-      <Top />
-      <RouterIndex />
-      <Footer />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Top />
+        <RouterIndex />
+        <Footer />
+      </div>
+    </Provider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import rootReducer from "./reducers";
-import {Provider} from "react-redux";
-import {createStore} from "redux";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-const store = createStore(rootReducer);
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change

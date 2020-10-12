aa395540ff5c6f15a65aaa97566675fe9c892fdf
Weijing Lin <weijingjaylin@gmail.com>
2020-10-12 09:40:42 -0700

    check email on list-add

--- src/components/home/list-add.js
+++ src/components/home/list-add.js
@@ -3,10 +3,16 @@ import {
   Button,
   Card,
   CardContent,
+  Input,
   TextField,
   Typography,
+  FormHelperText,
+  FormControl,
 } from "@material-ui/core";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import Axios from "axios";
 const useStyles = makeStyles((theme) =>
   createStyles({
     root: {
@@ -17,15 +23,66 @@ const useStyles = makeStyles((theme) =>
     },
   })
 );
-let email = localStorage.getItem("email") || "jay.lin@dkwholesale.us";
+
+const emailHelpText = {
+  LOGIN_DEFAULT: "You can't modify this email field once you are login",
+  NOLOGIN_DEFAULT: "Please enter your email, so other people can reach you.",
+  NOLOGIN_EXITED_EMAIL:
+    "This email is registered by other user, please use other email",
+};
+
 export default function ListAdd({ onSubmit }) {
+  let email = "name@domain.com";
+  let user = useSelector(({ auth }) => auth);
+  let timeout;
   const classes = useStyles();
+  // console.log("before useState email", email, user);
   const [formData, setFormData] = useState({
     email,
   });
-  console.log(formData);
+  const [disableEmail, setDisableEmail] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  useEffect(() => {
+    // console.log("useEffect user", user);
+    if (user) {
+      email = user.email;
+      setDisableEmail(true);
+    } else if (localStorage.getItem("email")) {
+      email = localStorage.getItem("email");
+      setDisableEmail(false);
+    } else {
+      setDisableEmail(false);
+    }
+    setFormData({ email });
+  }, [user]);
+
+  useEffect(() => {
+    if (!user) {
+      // console.log("initial checking email", email);
+      checkIfExistedEmail(email);
+    }
+  }, []);
+  // console.log("after setState email", formData);
+  // const user = useSelector(({ auth }) => auth);
+  // console.log("ListAdd", user);
+
+  const checkIfExistedEmail = (newEmail) => {
+    Axios.get(`http://api.ppewholesale.us:3030/users/${newEmail}?by=email`)
+      .then((rsp) => {
+        // console.log("api rsp", rsp);
+        if (rsp.data.uid) {
+          setEmailError(true);
+        } else {
+          setEmailError(false);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
   const handleChange = (name) => (e) => {
-    console.log("handleChange", name, e.target.value);
+    // console.log("handleChange", name, e.target.value);
     switch (name) {
       case "unit":
         formData[name] = parseInt(e.target.value);
@@ -34,20 +91,28 @@ export default function ListAdd({ onSubmit }) {
         formData[name] = parseFloat(e.target.value);
         break;
       case "email":
-        localStorage.setItem("email", e.target.value);
+        clearTimeout(timeout);
+        const newEmail = e.target.value;
+        timeout = setTimeout(() => {
+          // console.log("timeout");
+          localStorage.setItem("email", newEmail);
+          checkIfExistedEmail(newEmail);
+        }, 500);
+
       // thorow
       default:
         formData[name] = e.target.value;
     }
-
+    // console.log("handleFormChange", formData);
     setFormData(formData);
   };
   const handleSubmit = (e) => {
     // console.log(e);
-    console.log(formData);
+    // console.log(formData);
     formData["lastUpdated"] = Math.floor(Date.now() / 1000);
     onSubmit(formData);
   };
+  // console.log("before render email", email, formData);
   return (
     <Card variant="outlined" style={{ marginBottom: "20px" }}>
       <CardContent>
@@ -87,11 +152,24 @@ export default function ListAdd({ onSubmit }) {
           </div>
           <Typography variant="subtitle1">Contact</Typography>
           <div>
-            <TextField
-              label="Email"
-              defaultValue={formData["email"]}
-              onChange={handleChange("email")}
-            />
+            {disableEmail ? (
+              <FormControl style={{ margin: "8px" }}>
+                <Input disabled label="Email" value={formData["email"]} />
+                <FormHelperText>{emailHelpText.LOGIN_DEFAULT}</FormHelperText>
+              </FormControl>
+            ) : (
+              <TextField
+                label="Email"
+                error={emailError}
+                defaultValue={localStorage.getItem("email") || ""}
+                onChange={handleChange("email")}
+                helperText={
+                  emailError
+                    ? emailHelpText.NOLOGIN_EXITED_EMAIL
+                    : emailHelpText.NOLOGIN_DEFAULT
+                }
+              />
+            )}
           </div>
 
           <Button type="submit">Post</Button>

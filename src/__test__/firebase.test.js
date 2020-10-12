24e459f15408dd0606db81c9493a55de4f8cd1ed
Weijing Lin <weijingjaylin@gmail.com>
2020-10-12 11:13:41 -0700

    add firebase test

---
+++ src/__test__/firebase.test.js
@@ -0,0 +1,7 @@
+import { TextsmsTwoTone } from "@material-ui/icons";
+import firebase from "firebase/app";
+import firebaseConfig from "../config/firebase";
+
+test("initialize firebase", () => {
+  // firebase.initializeApp(firebaseConfig);
+});

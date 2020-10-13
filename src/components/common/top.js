8d88454b5d98256870fa3a7e93f8cac07ffe7512
Weijing Lin <weijingjaylin@gmail.com>
2020-10-13 15:56:32 -0700

    create mobilecontainer for navigation bar

--- src/components/common/top.js
+++ src/components/common/top.js
@@ -1,4 +1,12 @@
-import { Link, Grid, Typography, IconButton } from "@material-ui/core";
+import {
+  Link,
+  Grid,
+  Typography,
+  IconButton,
+  Container,
+  useMediaQuery,
+  useTheme,
+} from "@material-ui/core";
 import AppBar from "@material-ui/core/AppBar";
 import LoginDialog from "./LoginDialog";
 import React, { useState } from "react";
@@ -7,6 +15,11 @@ import Toolbar from "@material-ui/core/Toolbar";
 import { useSelector } from "react-redux";
 import UserAvatar from "../profile/userAvatar";
 
+function MobileContainer(props) {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("sm"));
+  return matches ? <Container>{props.children}</Container> : props.children;
+}
 export default function Top() {
   const auth = useSelector((state) => state.auth);
   const [dialogOpen, setDialogOpen] = useState(false);
@@ -16,39 +29,46 @@ export default function Top() {
   return (
     <AppBar position="fixed" className="no-print">
       <Toolbar>
-        <Grid container style={{ justifyContent: "space-between" }}>
-          <Grid item md={6} style={{ display: "flex", alignItems: "center" }}>
-            <IconButton>
-              <SupervisedUserCircleIcon
+        <MobileContainer>
+          <Grid container style={{ justifyContent: "space-between" }}>
+            <Grid item md={6} style={{ display: "flex", alignItems: "center" }}>
+              <IconButton>
+                <SupervisedUserCircleIcon
+                  style={{
+                    color: "white",
+                  }}
+                />
+              </IconButton>
+              <Link
+                href="#"
+                color="inherit"
                 style={{
-                  color: "white",
+                  textDecoration: "none",
                 }}
-              />
-            </IconButton>
-            <Link
-              href="#"
-              color="inherit"
+              >
+                <Typography
+                  variant="h6"
+                  color="inherit"
+                  noWrap
+                  display="inline"
+                >
+                  PPE Wholesale &lt;3 US
+                </Typography>
+              </Link>
+            </Grid>
+            <Grid
+              item
+              md={6}
               style={{
-                textDecoration: "none",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-end",
               }}
             >
-              <Typography variant="h6" color="inherit" noWrap display="inline">
-                PPE Wholesale &lt;3 US
-              </Typography>
-            </Link>
+              <UserAvatar user={auth} />
+            </Grid>
           </Grid>
-          <Grid
-            item
-            md={6}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-end",
-            }}
-          >
-            <UserAvatar user={auth} />
-          </Grid>
-        </Grid>
+        </MobileContainer>
       </Toolbar>
       <LoginDialog open={dialogOpen} onClose={handleClose} />
     </AppBar>
